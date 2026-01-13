2cfd737d54aeaeebffe5cbcceb3d310fad03fecc
Chirag <chiraggit1@gmail.com>
2026-01-13 20:52:00 +0530

    initial  commit

---
+++ Day 1 - Working with variables in python to manage data/Project - The Band Name Generator.py
@@ -0,0 +1,16 @@
+# The Band Name Generator
+# This program generates a band name using the user's city and pet name
+
+# Display a welcome message
+print("Welcome to the Band Name Generator")
+
+# Ask the user for the name of the city they grew up in
+city = input("What's the name of the city you grew up in? ")
+print(city)  # Print the entered city
+
+# Ask the user for the user's pet name
+pet = input("What's your pet name? ")
+print(pet)  # Print the entered pet name
+
+# Create and display the band name by combining city and pet name
+print("Your Band Name could be " + city + " " + pet)
