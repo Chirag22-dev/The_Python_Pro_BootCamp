64fb78d5cf309d3c6bc073df1c468bcdcb3b4039
Chirag <chiraggit1@gmail.com>
2026-01-13 18:51:26 +0530

    initial  commit

---
+++ Day 1 - Working with variables in python to manage data/02 String Manipulation and Code Intelligence.py
@@ -0,0 +1,7 @@
+"""Using String Concatenation and the new line escape sequence to format strings in python."""
+
+print("Hello World\nHello World\nHello World") # Here \n is used to add another line.
+
+print("Hello"+" "+"PlayBoy") # Here + is used to concatenate the strings
+
+
