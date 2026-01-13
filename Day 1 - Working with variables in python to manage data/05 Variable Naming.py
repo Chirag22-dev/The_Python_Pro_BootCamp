1b14b8e2655255395cbbec7505edcd75cebb96aa
Chirag <chiraggit1@gmail.com>
2026-01-13 20:25:05 +0530

    initial  commit

---
+++ Day 1 - Working with variables in python to manage data/05 Variable Naming.py
@@ -0,0 +1,14 @@
+""" Variable names must start with a letter or underscore, contain only letters, numbers,
+ and underscores, and must not be Python keywords."""
+
+# Ask the user to enter their username and store it in the variable 'user_name'
+user_name = input("Enter your username: ")
+
+# Calculate the number of characters in the username
+length = len(user_name)
+
+# Print the username entered by the user
+print(user_name)
+
+# Print the length of the username
+print(length)
