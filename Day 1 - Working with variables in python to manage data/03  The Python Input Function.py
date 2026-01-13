2082935cd4be1902f00ca208c14a5b6f3b1d02f9
Chirag <chiraggit1@gmail.com>
2026-01-13 19:15:06 +0530

    initial  commit

--- Day 1 - Working with variables in python to manage data/03  The Python Input Function.py
+++ Day 1 - Working with variables in python to manage data/03  The Python Input Function.py
@@ -1,1 +1,14 @@
 print("Hello "+ input("Enter your name:")+"!")
+
+"""input("Enter your name:")
+→ asks the user to enter their name and takes it as text (string)
+
+"Hello " + ... + "!"
+→ joins (concatenates) three strings:
+
+"Hello "
+the name entered by the user
+"!"
+
+print(...)
+→ displays the final message on the screen"""
