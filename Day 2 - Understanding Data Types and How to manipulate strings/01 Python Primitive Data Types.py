79debdcac5764540b6ffe257438f89411f778402
Chirag <chiraggit1@gmail.com>
2026-01-16 20:07:54 +0530

    initial  commit

---
+++ Day 2 - Understanding Data Types and How to manipulate strings/01 Python Primitive Data Types.py
@@ -0,0 +1,18 @@
+# Subscripting
+print("Hello"[0])
+
+# String
+print("123"+"345")
+
+# Integer = whole number
+print(123+456)
+
+# Large Integers
+print(123_345_678)
+
+# Floating Point Number
+print(3.14)
+
+# Boolean
+print(True)
+print(False)
