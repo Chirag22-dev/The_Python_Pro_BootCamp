9471e91dc078882efc9e4e5dfbbfde7b4ed7b8be
Chirag <chiraggit1@gmail.com>
2026-01-21 22:43:13 +0530

    initial  commit

---
+++ Day 4- Randomisation and Python Lists/04 Nested Lists.py
@@ -0,0 +1,14 @@
+# List of fruits
+fruits = ["apple", "banana", "cherry"]
+
+# List of vegetables
+vegetables = ["carrot", "beetroot", "Raddish"]
+
+# Create a nested list (list inside another list)
+nest_lists = [fruits, vegetables]
+
+# Print the complete nested list
+print(nest_lists)
+
+# Access the 3rd element (index 2) from the first list (fruits)
+print(nest_lists[0][2])
